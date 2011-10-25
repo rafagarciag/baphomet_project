a25efe68a3a7fb8d214fca2bf22b09dfc5db7f0d
Rafael Garcia <rafagarciag90@gmail.com>
2011-10-25 02:04:53 -0500

    The baphomet is here

---
+++ cl2p/app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+	def index
+		#Code for home page
+		@message = "THE FUTURE OF PASTEBINS IS HERE"
+	end
+end
---
+++ cl2p/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cl2p::Application.config.secret_token = '3124f73646d697838bb611e4993137c89f5af0126255094b02a316e0a4aa23b8acf3bec9fa8b777dbdf76fec9169520c1fe726de973f33c332b5e7b68ec7eabe'
