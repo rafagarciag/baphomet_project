c1b8610a9e5f726354ca34b46bd0652940de5cb9
jblancosegura <jblancosegura@gmail.com>
2011-12-07 13:07:12 -0600

    added devise

---
+++ cl2p/app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+
+  ################# DEVISE BLOCK #################
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  ################################################
+
+
+  ################# ATTRIBUTES BLOCK #################
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me, #Obligatory and Devise Fields
+		:age, :gender, :country, :whyUse #Optional Fields
+  ####################################################
+
+
+  ################# RELATIONSHIPS BLOCK #################
+  has_many :pastebins
+  #######################################################
+
+
+  ################# VALIDATIONS BLOCK #################
+  validates_presence_of :username, :message => ": Username missing. Please add one."
+  #####################################################
+
+end
