c9a70f484776c899b43fb5e3d8af9e22d9c210e5
jblancosegura <jblancosegura@gmail.com>
2012-01-24 19:58:46 -0600

    migra y permisos

--- cl2p/db/schema.rb
+++ cl2p/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120118225806) do
+ActiveRecord::Schema.define(:version => 20120125004438) do
 
   create_table "pastebins", :force => true do |t|
     t.string   "url"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120118225806) do
     t.datetime "updated_at"
     t.boolean  "editable",   :default => true
     t.boolean  "visible",    :default => true
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
