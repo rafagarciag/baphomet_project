4945f9ac3b90f179c2d2047d011c36d22f74089f
jblancosegura <jblancosegura@gmail.com>
2012-02-02 17:49:12 -0600

    Tweet button

--- cl2p/app/controllers/users/omniauth_callbacks_controller.rb
+++ cl2p/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  # Retrieves the facebook data
   def facebook
-    # You need to implement the method below in your model
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
--- cl2p/app/models/user.rb
+++ cl2p/app/models/user.rb
@@ -23,6 +23,9 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :message => " is missing. Please add one."
   #####################################################
 
+  # Notice that Devise RegistrationsController by default calls "User.new_with_session" before building a resource. 
+  # This means that, if we need to copy data from session whenever a user is initialized before sign up, 
+  # we just need to implement new_with_session in our model.
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
@@ -31,6 +34,7 @@ class User < ActiveRecord::Base
     end
   end
 
+  # Finds or creates a user who logs in using facebook
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
   	data = access_token.extra.raw_info
   	if user = User.where(:email => data.email).first
@@ -38,14 +42,6 @@ class User < ActiveRecord::Base
   	else # Create a user with a stub password. 
     		User.create!(:username => data.name, :email => data.email, :password => Devise.friendly_token[0,20]) 
   	end
-  end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"]
-      end
-    end
   end
 
 end
