8e67069850a04726c2a00c26a3e8fb5351323f97
jblancosegura <jblancosegura@gmail.com>
2012-02-16 17:11:19 -0600

    se cambio el content por BLoB

--- cl2p/db/schema.rb
+++ cl2p/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120125022104) do
+ActiveRecord::Schema.define(:version => 20120216230445) do
 
   create_table "pastebins", :force => true do |t|
     t.string   "url"
-    t.string   "content"
+    t.binary   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "editable",   :default => true
